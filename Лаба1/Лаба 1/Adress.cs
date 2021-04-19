9b41f0396cf418b29fe5ff084c234c0653c40f95
PAWKEY <letunovskiy.andrey73@mail.ru>
2021-04-19 09:45:07 +0300

    Create Project!

---
+++ Лаба1/Лаба 1/Adress.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Лаба_1
+{
+    class Adress
+    {
+        public string city { get; set; }
+        public string street { get; set; }
+        public int dom { get; set; }
+      
+
+        public Adress(string city, string street, int dom)
+        {
+            this.city = city;
+            this.street = street;
+            this.dom = dom;
+        }
+
+        public Adress(Adress a) : this(a.city, a.street, a.dom) { }
+
+
+    }
+
+}
---
+++ Лаба1/Лаба 1/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Лаба_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Adress> adresik = new List<Adress>();
+            string city;
+            string street;
+            int dom;
+            for (int i = 0; i < 3; i++)
+            {
+                Console.Write("Введите город: ");
+                city = Console.ReadLine();
+                Console.Write("Введите улицу: ");
+                street = (Console.ReadLine());
+                Console.Write("Введите номер дома: ");
+                dom = int.Parse(Console.ReadLine());
+                adresik.Add(new Adress(city, street, dom));
+                Console.WriteLine("-------------------------------------------------: ");
+            }
+
+            Console.WriteLine("Вывод списка");
+            foreach (Adress a in adresik)
+            {
+                Console.WriteLine("Город: " + a.city);
+                Console.WriteLine("Улица: " + a.street);
+                Console.WriteLine("Номер дома: " + a.dom);
+                Console.WriteLine("=======");
+
+            }
+
+            Console.ReadLine();
+
+        }
+    }
+}
